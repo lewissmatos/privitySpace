565e41f339c26aa101f8f47e0b85349bfbbc401f
Lewis Matos <lewiismb1@gmail.com>
2021-04-16 19:12:46 -0400

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './dashboard/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'dashboard/home'},
+  {path: 'dashboard/home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
