3c071289e5e311a3515f7eaa01d0a5bd1ccbc58b
unknown <lewiismb1@gmail.com>
2021-04-25 09:31:28 +0200

    Primer commit

--- src/app/dashboard/components/home/home.component.ts
+++ src/app/dashboard/components/home/home.component.ts
@@ -9,7 +9,38 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  activatedH = false
+  activatedC = false
+  activatedS = false
+  activatedP = false
+
   ngOnInit(): void {
+    this.activatedH = true
+  }  
+
+  h() {
+    this.activatedH = true
+    this.activatedC = false
+    this.activatedS= false
+    this.activatedP = false
+  }
+  c() {
+    this.activatedH = false
+    this.activatedC = true
+    this.activatedS= false
+    this.activatedP = false
+  }
+  s() {
+    this.activatedH = false
+    this.activatedC = false
+    this.activatedS= true
+    this.activatedP = false
+  }
+  p() {
+    this.activatedH = false
+    this.activatedC = false
+    this.activatedS= false
+    this.activatedP = true
   }
 
 }
